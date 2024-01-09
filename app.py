2030f039d4d3ec22fcfebad6f8ae700a8c974aea
JulioFreitas <juliosparapan2014@gmail.com>
2024-01-09 16:47:57 -0300

    Adicionado lib pydantic e metodo POST

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
+from pydantic import BaseModel
 
 server = Flask(__name__)
+spec = FlaskPydanticSpec('Flask', title='API PYTHON')
+spec.register(server)
+
+class Pessoa(BaseModel):
+    id: int
+    nome: str
+    idade: int
 
 @server.get('/pessoas')
-def buscar_pessoas():
+@spec.validate(resp=Response(HTTP_200=Pessoa))
+def buscar_pessoa():
     return "retorno de uma pessoa"
+
+@server.post('/pessoas')
+@spec.validate(body=Pessoa, resp=Response(HTTP_200=Pessoa))
+def inserir_pessoa():
+    body = request.context.body.dic
+    return body 
+
 
 server.run()
