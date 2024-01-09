293fcc024b4fb1d02a43164cfdc9a5e4048c57f1
JulioFreitas <juliosparapan2014@gmail.com>
2024-01-09 18:31:56 -0300

    list e count no metodo GET

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
 from pydantic import BaseModel
@@ -10,14 +11,24 @@ spec.register(server)
 database = TinyDB('database.json')
 
 class Pessoa(BaseModel):
-    id: int
+    id: Optional[int]
     nome: str
     idade: int
 
+class Pessoas(BaseModel):
+    pessoas: list[Pessoa]
+    count: int
+
 @server.get('/pessoas')
-def buscar_pessoa():
+@spec.validate(resp=Response(HTTP_200=Pessoas))
+def buscar_pessoas():
     """ Busca todas pessoas no banco de dados"""
-    return jsonify(database.all())
+    return jsonify(
+        Pessoas(
+            pessoas=database.all(),
+            count=len(database.all())
+        ).dict()
+    )
 
 @server.post('/pessoas')
 @spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
