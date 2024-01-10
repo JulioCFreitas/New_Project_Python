7f14b871887fe011ec09f312002e44826db55745
JulioFreitas <juliosparapan2014@gmail.com>
2024-01-10 18:11:33 -0300

    Tratativa de exceção no metodo GET por ID

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from itertools import count
 from typing import Optional
 from flask import Flask, request, jsonify
@@ -36,17 +37,27 @@ def buscar_lista_pessoas():
         ).dict()
     )
 
-#Metodo GET
+# Método GET por ID
 @server.get('/pessoas/<int:id>')
-@spec.validate(resp=Response(HTTP_200=Pessoas))
-def buscar_pessoas():
-    """ Buscar pessoa pelo ID no DB"""
-    return jsonify(
-        Pessoas(
-            pessoas=database.all(),
-            count=len(database.all())
-        ).dict()
-    )
+@spec.validate(resp=Response(HTTP_200=Pessoa))
+def buscar_pessoas_id(id: int):
+    """Buscar pessoa por ID no DB"""
+    pessoa = database.search(Query().id == id)
+    if not pessoa:
+        return jsonify({'message': 'Pessoa not found!'}), 404
+    return jsonify(pessoa[0])
+
+# Método GET por ID
+# @server.get('/pessoas/<int:id>')
+# @spec.validate(resp=Response(HTTP_200=Pessoas))
+# def buscar_pessoas_id(id):
+#     """ Buscar pessoa por ID no DB"""
+#     try:
+#         pessoa = database.search(Query().id == id)[0]
+#     except IndexError:
+#         return {'message':'Pessoa not found!'}
+#     return jsonify(pessoa)
+
 
 #Metodo POST
 @server.post('/pessoas')
@@ -57,15 +68,16 @@ def inserir_pessoa():
     database.insert(body)
     return body 
  
-#Metodo PUT
+# Método PUT
 @server.put('/pessoas/<int:id>')
-@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_201=Pessoa))
 def editar_pessoa(id):
     """Editar pessoa pelo ID no DB"""
-    Pessoa = Query()
+    pessoa_query = Query()
     body = request.context.body.dict()
-    database.update(body, Pessoa.id == id)
+    database.update(body, pessoa_query.id == id)
     return jsonify(body)
+
 
 #Metodo DELET
 @server.delete('/pessoas/<int:id>')
